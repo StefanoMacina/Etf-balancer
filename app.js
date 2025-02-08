33401c5f6b40b6848fc61dd162052f9a5bb4a84a
stefano <maclion@live.it>
2025-02-08 11:55:45 +0100

    add etf modal add

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ import { CalcContainer } from "./components/CalcContainer.js";
 import { CalcMain } from "./components/CalcMain.js";
 import { CalcSidebar } from "./components/CalcSidebar.js";
 import InputComponent from "./components/InputComponent.js";
+import { ModalComponent } from "./components/Modal.js";
 
 customElements.define("calc-container", CalcContainer);
 customElements.define("calc-sidebar", CalcSidebar);
 customElements.define("calc-main", CalcMain);   
 customElements.define("input-group", InputComponent);
+customElements.define("modal-component", ModalComponent);
--- components/CalcSidebar.js
+++ components/CalcSidebar.js
@@ -1,4 +1,5 @@
 export class CalcSidebar extends HTMLElement {
+  modalId =  "add-etf-modal";
   constructor() {
     super();
     
@@ -13,8 +14,11 @@ export class CalcSidebar extends HTMLElement {
     <div class="container p-4">
         <input-group title="Current PAC value" icon="$" ></input-group>
         <input-group title="Increment" icon="$" ></input-group>
-        <button type="button" class="btn btn-primary btn-md rounded-3 d-block mx-auto">ADD ETF</button>
+        <button type="button" data-bs-toggle="modal" data-bs-target="#${this.modalId}" class="btn btn-primary btn-md rounded-3 d-block mx-auto">ADD ETF</button>
     </div>
+    <modal-component title="Add ETF" modal-id="${this.modalId}">
+      <h1>im here</h1>
+    </modal-component>
     `
   }
 
