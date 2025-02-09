3c6a7cf2146d3bad9b3533e23edf2ef96f04889d
stefano <maclion@live.it>
2025-02-10 00:42:28 +0100

    last commit

--- components/CalcContainer.js
+++ components/CalcContainer.js
@@ -10,12 +10,12 @@ export class CalcContainer extends HTMLElement {
 
   template = () => {
     return /*html*/ `
-      <section class="border rounded-4 w-75 vh-100 mx-auto my-2" style="--bs-border-opacity: .5">
+      <section class="border rounded-4 w-75 mx-auto  mt-4" style="--bs-border-opacity: .5; height: 50vh" >
         <div class="row h-100 g-0">
-          <div class="col-4">
+          <div class="col-3">
             <calc-sidebar></calc-sidebar>
           </div>
-          <div class="col-8 border-start">
+          <div class="col-9 border-start">
             <calc-main></calc-main>   
           </div>
         </div>
